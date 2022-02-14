a1dce0fb75e0690f66045ef55efac049231b412a
Josiah <Jsamuels@student.sjcny.edu>
2022-02-14 17:15:31 -0500

    Added to Yearbook2 and Graduation2

--- app/src/main/java/com/example/ouryearbook/Yearbook2.java
+++ app/src/main/java/com/example/ouryearbook/Yearbook2.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class Yearbook2 extends AppCompatActivity {
 
@@ -15,8 +16,8 @@ public class Yearbook2 extends AppCompatActivity {
         setContentView(R.layout.activity_yearbook2);
 
         Button yearbook2Btn1 = (Button) findViewById(R.id.yearbook2Btn1);
-
-
+        Button btnChangeText= (Button) findViewById(R.id.btnevent);
+        Button btnGraduation= (Button) findViewById(R.id.btnGraduate);
 
 
         //this is how you get yearbook2 to return back to the dashboard
@@ -25,11 +26,33 @@ public class Yearbook2 extends AppCompatActivity {
             public void onClick(View v) {
                 Intent i = new Intent(getApplicationContext(), MainActivity.class);
                 startActivity(i);
+
+
             }
         });
 
+        btnChangeText.setOnClickListener(new View.OnClickListener(){
+       @Override
+          public void onClick(View view){
+            final TextView textView  =(TextView) findViewById(R.id.KilluaText);
+             textView.setText("The one who’s cursed is the one making wishes!” ");
 
 
 
-    }
+                                             }
+
+
+                                         });
+
+
+                btnGraduation.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent i = new Intent(getApplicationContext(), graduation2.class);
+                        startActivity(i);
+
+
+                    }
+                });
+}
 }
