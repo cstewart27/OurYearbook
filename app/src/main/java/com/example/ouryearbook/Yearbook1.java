f727f6d9ed602a6782d55be6c8bf6d82eed8d817
cstewart27 <cstewart3@student.sjcny.edu>
2022-02-19 15:43:39 -0500

    share image working

--- app/src/main/java/com/example/ouryearbook/Yearbook1.java
+++ app/src/main/java/com/example/ouryearbook/Yearbook1.java
@@ -3,14 +3,20 @@ package com.example.ouryearbook;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class Yearbook1 extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,10 +55,15 @@ public class Yearbook1 extends AppCompatActivity {
         ImageBtn1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                ImageView imgView1 = (ImageView) findViewById(R.id.imageView);
+                BitmapDrawable drawable = (BitmapDrawable)imgView1.getDrawable();
+                Bitmap bitmap = drawable.getBitmap();
+                String bitmapPath = MediaStore.Images.Media.insertImage(getContentResolver(), bitmap, "LegoBatman", null);
+                Uri uri = Uri.parse(bitmapPath);
                 Intent myIntent = new Intent();
                 myIntent.setAction(Intent.ACTION_SEND);
                 myIntent.setType("image/jpeg");
-                myIntent.putExtra(Intent.EXTRA_STREAM, LegoBatman);
+                myIntent.putExtra(Intent.EXTRA_STREAM, uri);
                 startActivity(Intent.createChooser(myIntent, "Share Image Using:"));
             }
         });
