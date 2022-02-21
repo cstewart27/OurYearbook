37bee1a41c2123386de740e3ee030febe9dfb846
francocarlos011 <carlosafrancof@gmail.com>
2022-02-21 17:38:49 -0500

    Carlos F

--- app/src/main/java/com/example/ouryearbook/Yearbook4.java
+++ app/src/main/java/com/example/ouryearbook/Yearbook4.java
@@ -2,7 +2,11 @@ package com.example.ouryearbook;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class Yearbook4 extends AppCompatActivity {
 
@@ -10,5 +14,39 @@ public class Yearbook4 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_yearbook4);
+
+        Button yearbookdashboard = (Button) findViewById(R.id.btndashboard);
+        Button btntext= (Button) findViewById(R.id.btntext);
+        Button btnplan= (Button) findViewById(R.id.btnplan);
+
+        btntext.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+            final TextView text  =(TextView) findViewById(R.id.txttext);
+        text.setText("I play Tennis” ");
+
+            }
+        });
+
+        btnplan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), graduation4.class);
+                startActivity(i);
+
+
+            }
+        });
+
+        yearbookdashboard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(i);
+
+
+            }
+        });
     }
+
 }
